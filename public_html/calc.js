16e0e63db13010a5956c1d790db397e57faefa32
piwatake <piwatake2011@knights.ucf.edu>
2015-08-04 17:50:37 -0400

    Added error messages for invalid input, fixed some errors.

--- public_html/calc.js
+++ public_html/calc.js
@@ -6,21 +6,73 @@
 
 
     function calcSal(){
-                    $(document.getElementById('mockery')).hide();
-                    
-                    var calc = (Math.round(document.getElementById('hourlywageref').value * document.getElementById('hoursperdayref').value 
-                            * document.getElementById('daysperweekref').value * 4 * 12));
-                    
-                    $(document.getElementById('mockery')).html(calc).fadeIn(500) ;
-                    
-                }
+        $(document.getElementById('hrsErrsal')).hide();
+        $(document.getElementById('wageErrsal')).hide();
+        $(document.getElementById('daysErrsal')).hide();
+        $(document.getElementById('mockery')).hide();
+        
+        var myWagehr = document.getElementById('hourlywageref').value;
+        var myHrsWork = document.getElementById('hoursperdayref').value ;
+        var myDaysWork = document.getElementById('daysperweekref').value;
+        
+        if (!isValidNum(myWagehr)){
+            $(document.getElementById('wageErrsal')).show();
+        }
+        if (!isValidNum(myHrsWork)) {
+            $(document.getElementById('hrsErrsal')).show();
+            console.log("unhidden.\n");
+        }
+        if (!isValidNum(myDaysWork)){
+            $(document.getElementById('daysErrsal')).show();
+        }
+
+        var calc = (Math.round(myWagehr * myHrsWork
+                * myDaysWork * 4 * 12));
+
+        $(document.getElementById('mockery')).html(calc).fadeIn(500) ;
+
+    }
                 
     function calcSalHr(){
+       $(document.getElementById('hrsErrhr')).hide();
+       $(document.getElementById('salErrhr')).hide();
+       $(document.getElementById('daysErrhr')).hide();
+       $(document.getElementById('mockperhour')).hide();
+       
+       console.log("hidden.\n");
         
-        $(document.getElementById('mockperhour')).hide();
+        
+        var myYearSal = document.getElementById('yearlysalref').value;
+        var myHrsWork = document.getElementById('hoursperdayrefhr').value ;
+        var myDaysWork = document.getElementById('daysperweekrefhr').value;
+        
+        if (!isValidNum(myHrsWork)) {
+            $(document.getElementById('hrsErrhr')).show();
+            console.log("unhidden.\n");
+        }
+        if (!isValidNum(myYearSal)){
+            $(document.getElementById('salErrhr')).show();
+        }
+        if (!isValidNum(myDaysWork)){
+            $(document.getElementById('daysErrhr')).show();
+        }
+        
                     
-                    var calc = (Math.floor(document.getElementById('yearlysalref').value / 1920));
+                    var calc = (Math.floor( myYearSal / (myHrsWork * myDaysWork * 4 * 12)));
                     
                     $(document.getElementById('mockperhour')).html(calc).fadeIn(500) ;
+    
+    }
+    
+    
+    function isValidNum(tocheck){
+        
+        if (tocheck.match(/^([0-9]\d*)$/)){
+             return true;
+            
+        } else {
+            
+            return false;
+        }
     
     }
